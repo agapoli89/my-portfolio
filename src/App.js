5cc07fde8ae894b2fab5ab338ca4c770abf0eea2
Agnieszka Polińska <agapoli@gmail.com>
2021-07-29 18:27:35 +0200

    Implement LangButton and Main Content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import { useState } from 'react';
+
+import LangButton from './LangButton/LangButton';
+import MainContent from './Components/MainContent/MainContent';
+
+import './App.scss';
+
 
 function App() {
+  const [lang, setLang] = useState('PL');
+
+  const handleChangeLang = () => setLang(prev => prev === 'PL' ? 'EN' : 'PL')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <LangButton lang={lang} handleChangeLang={handleChangeLang}/>
+      <MainContent lang={lang}/>
+      {/*         <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        > 
           Learn React
-        </a>
-      </header>
+        </a> */}
     </div>
   );
 }
