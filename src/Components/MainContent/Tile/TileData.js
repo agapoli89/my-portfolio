c4f81c497e4fc8cc8e77cf18c3d3a5dce24148eb
Agnieszka Polińska <agapoli@gmail.com>
2021-07-29 18:28:43 +0200

    Refill data

---
+++ src/Components/MainContent/Tile/TileData.js
@@ -0,0 +1,62 @@
+export const tileData = {
+    pl: [
+        {   
+            id: 1,
+            title: 'Słowo o mnie',
+            img: 'aboutme',
+            content: {
+                header: 'O mnie',
+                par1: 'Miłośniczka psów, rowerów miejskich, kuchni roślinnej i górskich wędrówek.',
+                par2: 'Mimo, że wykształcenie mam humanistyczne, coraz bardziej pochłania mnie programowanie. Potrafię spędzać godziny analizując i udoskonalając kod, co jak do tej pory sprawia mi olbrzymią frajdę :)',
+                subtitle: 'Technologie i umiejętności',
+            }
+        },
+        {
+            id: 2,
+            title: 'Dzień dobry! Fajnie, że jesteś :)',
+            additionalClass: 'tile--withoutImg',
+            additionalTextClass: 'tile__text--withoutImg',
+        },
+        {
+            id: 3,
+            title: 'Moje prace',
+            img: 'works',
+            additionalClass: 'tile--outstanding',
+        },
+        {
+            id: 4,
+            title: 'Kontakt',
+            img: 'phone',
+        },
+    ],
+    en: [
+        {   
+            id: 1,
+            title: 'About me',
+            img: 'aboutme',
+            content: {
+                header: 'About me',
+                par1: 'Dogs, city bikes, plant-based cousine and hiking lover.',
+                par2: "Though I always thought I have a humanist's soul I feel great in programming. I could spend hours analysing or improving code and I really enjoy it :)",
+                subtitle: 'Technologies and skills',
+            }
+        },
+        {
+            id: 2,
+            title: 'Hello! Nice to see You :)',
+            additionalClass: 'tile--withoutImg',
+            additionalTextClass: 'tile__text--withoutImg',
+        },
+        {
+            id: 3,
+            title: 'My projects',
+            img: 'works',
+            additionalClass: 'tile--outstanding',
+        },
+        {
+            id: 4,
+            title: 'Contact',
+            img: 'phone',
+        },
+    ]
+}
