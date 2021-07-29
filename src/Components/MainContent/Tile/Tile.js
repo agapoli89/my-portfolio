b20c1f9b9bf6d179759027d25647fee06b7112be
Agnieszka Polińska <agapoli@gmail.com>
2021-07-29 18:29:31 +0200

    feat: Tile

---
+++ src/Components/MainContent/Tile/Tile.js
@@ -0,0 +1,28 @@
+import aboutme from '../../../images/aboutme.png';
+import phone from '../../../images/phone.png';
+import works from '../../../images/works.png';
+
+import './Tile.scss';
+
+const Tile = ({id, title, img, additionalClass, additionalTextClass, handleVisibledContent}) => {
+    const image = img && (
+        <img 
+            src={img === 'aboutme'
+                    ? aboutme
+                    : img === 'phone'
+                        ? phone
+                        : works}
+            className="tile__image" 
+            alt={title} 
+        />
+    )
+    
+    return (  
+        <button id={id} className={`tile ${additionalClass}`} onClick={handleVisibledContent}>
+            {image}
+            <h2 className={`tile__text ${additionalTextClass}`}>{title}</h2>
+        </button>
+    );
+}
+ 
+export default Tile;
